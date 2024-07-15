22b03eb911e51d6bb4cd231a680cca0a171d2f8d
keerthikadevi-s <keerthikadevis6@gmail.com>
2024-07-15 20:39:46 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors =  require("cors");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
 const connectMon = require("./connectmon.js");
 const restrictToLoggedIn = require("./middlewares/authmiddle.js");
 const logReqRes = require("./middlewares/middle.js");
-const adminRouter = require("./routes/admin.js")
-const manageProducts = require("./routes/product.js")
+const adminRouter = require("./routes/admin.js");
+const manageProducts = require("./routes/product.js");
 
 //Connecting MongoDB
-connectMon("mongodb://localhost:27017/e-commerce");
+connectMon(
+  "mongodb+srv://keerthikadevis6:Ivo1SvrCykyAxHoM@cluster0.cwzekei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 //Middlewares
 app.use(express.json());
@@ -27,16 +29,15 @@ app.use(cookieParser());
 
 //Admin functionalities
 //Admin creation - post request
-app.use("/admin",logReqRes("adminlog.txt"));
+app.use("/admin", logReqRes("adminlog.txt"));
 app.use("/admin", adminRouter);
 
 //Products - addition, updation, deletion
 // app.use("/products/update",express.json());
-app.use("/products",logReqRes("productlog.txt"));
+app.use("/products", logReqRes("productlog.txt"));
 app.use("/products/update", restrictToLoggedIn);
 app.use("/products", manageProducts);
 
-app.listen(PORT, ()=> {
-    console.log(`Backend server is running on http://localhost:${PORT}`);
-
-})
+app.listen(PORT, () => {
+  console.log(`Backend server is running on http://localhost:${PORT}`);
+});
